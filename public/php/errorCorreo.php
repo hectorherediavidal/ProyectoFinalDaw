34edefa41163148f332c2a60ca190f478a52ad55
Hector Heredia Vidal <hectorheredia@estudiante.edib.es>
2023-03-10 18:21:54 +0100

    añadido verificacion en php y comprobacion de correo existente

---
+++ public/php/errorCorreo.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExcuseParadise</title>
+    <style>
+        body {
+            background-color: #00BEDA;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100vh;
+        }
+
+        a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-family: "tipografia";
+            font-size: 20px;
+            color: black;
+            text-decoration: none;
+            text-align: center;
+            margin-left: 30px;
+            margin-right: 30px;
+            margin-top: 20px;
+            padding: 15px;
+            width: 150px;
+            height: 10px;
+            border-radius: 20px;
+            border: 3px solid black;
+            background-color: white;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>El correo que has introducido ya existe, por favor escribe un correo distinto</h1>
+
+    <a href="../paginasHTML/registro.html">Volver al registro</a>
+</body>
+
+</html>
--- public/php/recogerDatosRegistro.php
+++ public/php/recogerDatosRegistro.php
@@ -1,9 +1,7 @@
 <?php
 
 
-$nombre = $_POST['registroNombre'];
-$contraseña = $_POST['registroContraseña'];
-$correo = $_POST['registroCorreo'];
+
 
 $host = "localhost";
 $user = "edib";
@@ -20,8 +18,53 @@ try{
 }
 
 
-$sentencia = "INSERT INTO  usuarios(nombre, contraseña, correo) VALUES ( '$nombre', '$contraseña','$correo');";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre = ($_POST['registroNombre']);
+    $correo = ($_POST['registroCorreo']);
+    $contraseña = ($_POST['registroContraseña']);
+    $repetirContraseña = ($_POST['registroRepetirContraseña']);
+  
+    if (empty($nombre)) {
+      echo "El campo nombre está vacío.";
+    } elseif (!preg_match('/^[a-zA-Z ]+$/', $nombre)) {
+      echo "El nombre solo puede contener letras y espacios.";
+    }
+  
+    if (empty($correo)) {
+      echo "El campo correo está vacío.";
+    } elseif (!preg_match('/^[^\s@]+@[^\s@]+.[^\s@]+$/', $correo)) {
+      echo "El correo electrónico es inválido. Por favor, ingrese un correo electrónico válido.";
+    }
+  
+    if (empty($contraseña)) {
+      echo "El campo contraseña está vacío.";
+    } elseif (!preg_match('/^[a-zA-Z0-9]+$/', $contraseña)) {
+      echo "La contraseña solo puede contener letras y números.";
+    } elseif (strlen($contraseña) > 10) {
+      echo "La contraseña que has introducido es muy larga, solo puede contener 10 caracteres.";
+    }
+  
+    if (empty($repetirContraseña)) {
+      echo "Debe repetir la contraseña.";
+    } elseif ($repetirContraseña !== $contraseña) {
+      echo "Las contraseñas no coinciden.";
+    }
+  }
+
+  // Consulta para comprobar si ya existe el correo electrónico en la base de datos
+  $consulta = 'SELECT COUNT(*) FROM usuarios WHERE correo = :correo';
+  $sentencia = $conector->prepare($consulta);
+  $sentencia->bindParam(':correo', $correo, PDO::PARAM_STR);
+  $sentencia->execute();
+  $resultados = $sentencia->fetch(PDO::FETCH_NUM);
+
+  // Si el correo electrónico ya existe en la base de datos, mostrar un mensaje de error
+  if ($resultados[0] > 0) {
+      header('Location: ./errorCorreo.php');
+  } else {
+
+$insercion = "INSERT INTO  usuarios(nombre, contraseña, correo) VALUES ( '$nombre', '$contraseña','$correo');";
 $resultado = $conector->query($sentencia);
-
-
+header('Location: ../paginasHTML/login.html');
+  }
 $conn = null;
